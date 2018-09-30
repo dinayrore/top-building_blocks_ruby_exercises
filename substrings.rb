7f3c0e557ce469168526f2640b8aaae23674fa62
Kristine <kteich88@gmail.com>
2018-09-30 13:14:07 -0400

    created substrings

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,20 @@
 # substrings application
+def substrings(word, dictionary)
+  results = Hash.new
+  words = word.downcase.gsub(/[^a-z0-9\s]/i, '').split(' ')
+  words.each do |word|
+    if dictionary.include?(word) && results.key?(word) == false
+      results[word] = 1
+    end
+  end
+  puts results
+end
+
 def main
-
-
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  print "Provide a word to look up in the dictionary: "
+  word = gets.chomp
+  substrings(word, dictionary)
 end
 
 
